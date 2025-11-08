9b03855e7604121af4dbc4d119c8aec51cf1fe1f
Cerw3xo <149335179+Cerw3xo@users.noreply.github.com>
2025-11-08 20:24:49 +0100

    Update admin configuration to include cookie settings for rate limiting

--- dany-art-cmc/config/admin.ts
+++ dany-art-cmc/config/admin.ts
@@ -15,5 +15,5 @@ export default ({ env }) => ({
   transfer: { token: { salt: env('TRANSFER_TOKEN_SALT') } },
   secrets: { encryptionKey: env('ENCRYPTION_KEY') },
   flags: { nps: env.bool('FLAG_NPS', true), promoteEE: env.bool('FLAG_PROMOTE_EE', true) },
-  rateLimit: { enabled: true },
+  rateLimit: { enabled: true, cookie: { secure: false, sameSite: 'lax' }, },
 });
