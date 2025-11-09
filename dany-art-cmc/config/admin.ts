09bd3d8357f09ee803fb2068749aec5b88692c7f
Cerw3xo <149335179+Cerw3xo@users.noreply.github.com>
2025-11-09 18:14:33 +0100

    fix: add missing line in admin configuration for clarity

--- dany-art-cmc/config/admin.ts
+++ dany-art-cmc/config/admin.ts
@@ -4,6 +4,7 @@ export default ({ env }) => {
   return {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
+
     },
     apiToken: { salt: env('API_TOKEN_SALT') },
     transfer: { token: { salt: env('TRANSFER_TOKEN_SALT') } },
